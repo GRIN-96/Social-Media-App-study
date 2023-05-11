63a277cb4fc8ae32102e3122fb24e546aa061ef1
Carlos <bnmnil96@gmail.com>
2023-05-12 03:10:15 +0900

    complete front dev

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css"
+import Profile from "./components/Profile/Profile";
 import Auth from "./pages/Auth/Auth";
 import Home from "./pages/home/Home";
 function App() {
@@ -6,8 +7,8 @@ function App() {
     <div className="App">
       <div className="blur" style={{top : '-18%', right : "0"}}></div>
       <div className="blur" style={{top : "36%", left : "-8rem"}}></div>
-      <Home/>
-      {/* <Profile/> */}
+      {/* <Home/> */}
+      <Profile/>
       {/* <Auth /> */}
     </div>
   );
--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -4,6 +4,9 @@ import Cover from '../../img/cover.jpg'
 import Profile from '../../img/profileImg.jpg'
 
 const ProfileCard = () => {
+
+
+    const ProfilePage  = true;
   return (
     <div className="ProfileCard">
         <div className="ProfileImages">
@@ -28,10 +31,22 @@ const ProfileCard = () => {
                     <span>1</span>
                     <span>Followers</span>
                 </div>
+
+                {ProfilePage && (
+                    <>
+                        <div className="vl">
+
+                        </div>
+                        <div className="follow">
+                            <span>3</span>
+                            <span>Posts</span>
+                        </div>
+                    </>
+                )}
             </div>
             <hr />
         </div>
-
+            {ProfilePage ? "" : <span>My Profile</span>}
         <span>
             My Profile
         </span>
